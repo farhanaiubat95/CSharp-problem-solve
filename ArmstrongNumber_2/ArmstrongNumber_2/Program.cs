8600f6b9bb003e6a1d77c93c4692b1081e0e2bb4
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-11-05 18:20:59 +0600

    create  ArmstrongNumber_2,cs

---
+++ ArmstrongNumber_2/ArmstrongNumber_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ArmstrongNumber_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, temp,sum=0,rem;
+            Console.WriteLine("Please enter a number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+            temp=number;
+          
+            while (number != 0)
+            {
+                rem = number % 10;
+                sum = sum + (rem * rem * rem);
+                number /= 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("Yes, {0} is an Armstrong Number .",temp);
+            }
+            else
+            {
+                Console.WriteLine("No, {0} is not an Armstrong Number ..",temp);
+            }
+        }
+    }
+}
