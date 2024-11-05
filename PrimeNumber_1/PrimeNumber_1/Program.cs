3ac2a8f041395540b07b723ebb325800ec5be6ca
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-11-05 18:07:20 +0600

    Create PrimeNumber_1.cs

---
+++ PrimeNumber_1/PrimeNumber_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace PrimeNumber_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, flag = 0, i;
+            Console.WriteLine("Please enter a number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 2; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    flag = 1;
+                    i++;
+                }
+                else
+                {
+                    flag = 0;
+                    i++;
+                    
+                }
+               
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Yes, {0} is a prime number ", number);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No, {0} is not a prime number ", number);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
